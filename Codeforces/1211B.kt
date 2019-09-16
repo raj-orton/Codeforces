c9b62de5f7a4249f020806bb06a610a6d293f997
raj-orton <35659984+raj-orton@users.noreply.github.com>
2019-09-16 23:51:06 +0530

    Update 1211B.kt

--- Codeforces/1211B.kt
+++ Codeforces/1211B.kt
@@ -2,17 +2,17 @@ import java.util.*;
 val scan = Scanner(System.`in`);
  
 fun main() {
-    var n: Long = scan.nextLong();
-    var id: Long = -1;
-    var max: Long = -1;
-    for(i in 0..n - 1) {
-        var x: Long = scan.nextLong();
-        if(max <= x) {
-            id = i;
-            max = x;
-        }
-    }
-    id = id + 1;
-    var ans: Long = (n * max) - (n - id);
-    println(ans);
+	var n: Long = scan.nextLong();
+	var id: Long = -1;
+	var max: Long = -1;
+	for(i in 0..n - 1) {
+		var x: Long = scan.nextLong();
+		if(max <= x) {
+			id = i;
+			max = x;
+		}
+	}
+	id = id + 1;
+	var ans: Long = (n * max) - (n - id);
+	println(ans);
 }
